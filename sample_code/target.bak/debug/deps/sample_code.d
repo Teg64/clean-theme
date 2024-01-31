f973a85c13e0d8e1b9a96e9dbc6379049c7baaa1
Teg64 <teiganrosen@gmail.com>
2024-02-01 04:11:22 +1100

    Just some cleanup from accidentally pushing target

--- sample_code/target.bak/debug/deps/sample_code.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Teigan\Documents\projects_win\clean-theme\sample_code\target\debug\deps\sample_code.exe: src\main.rs src\sample_extra.rs
-
-C:\Users\Teigan\Documents\projects_win\clean-theme\sample_code\target\debug\deps\sample_code.d: src\main.rs src\sample_extra.rs
-
-src\main.rs:
-src\sample_extra.rs:
--- sample_code/target.bak/debug/sample_code.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Teigan\Documents\projects_win\clean-theme\sample_code\target\debug\sample_code.exe: C:\Users\Teigan\Documents\projects_win\clean-theme\sample_code\src\main.rs
